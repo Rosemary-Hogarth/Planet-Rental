b2567ee104e355afb33a90a435d0fc58f58b5007
Rosemary Hogarth <rohogarth@gmail.com>
2024-06-06 09:48:10 +0200

    Hid navbar and footer on homepage

--- app/javascript/controllers/homepage_controller.js
+++ app/javascript/controllers/homepage_controller.js
@@ -1,18 +1,44 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="homepage"
 export default class extends Controller {
   static targets = ["circle1", "column"];
 
   connect() {
-    console.log("Connected");
+    // Check if the current page is the homepage
+    if (window.location.pathname === "/") {
+      this.hideFooter();
+    }
+    if (window.location.pathname === "/") {
+      this.hideNavbar();
+    }
   }
+
   changeColor() {
-    this.circle1Target.style.backgroundColor = '#DEAC3D'
+    document.body.style.backgroundColor = '#ddd1bd';
+    this.circle1Target.style.backgroundColor = '#DEAC3D';
     this.columnTarget.style.backgroundColor = '#ddd1bd';
   }
-  changeColorBack () {
+
+  changeColorBack() {
     this.circle1Target.style.backgroundColor = '#e46023';
     this.columnTarget.style.backgroundColor = '#000';
+    document.body.style.backgroundColor = '#000';
+  }
+
+  hideFooter() {
+    // Get the footer element
+    const footer = document.querySelector("footer");
+    if (footer) {
+      // Hide the footer by setting its display property to none
+      footer.style.display = "none";
+    }
+  }
+
+  hideNavbar() {
+    const navbar = document.querySelector(".navbar");
+    if (navbar) {
+      navbar.style.display = "none";
+    }
   }
 }
