0c39f9e239158422c1496f30f240f3d56f4e2d0d
Jolenn Darensbourg <jolennd@gmail.com>
2024-06-08 10:06:09 +0200

    update dashboard controller to sort by creation date

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,7 +5,7 @@ class DashboardsController < ApplicationController
     # Fetch bookings for current user who booked or owns planet
     @bookings = Booking.joins(:planet).where("bookings.user_id = ? OR planets.user_id = ?", current_user.id,
                                              current_user.id)
-    # Fetch planets owned by current user
-    @planets = current_user.planets.uniq
+    # Fetch planets owned by current user and sort by creation date
+    @planets = current_user.owned_planets.order(created_at: :desc).uniq
   end
 end
