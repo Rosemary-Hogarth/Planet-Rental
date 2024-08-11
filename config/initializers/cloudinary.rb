a393d21d86d0e0128e434455abb52cfc67295dc1
Rosemary Hogarth <rohogarth@gmail.com>
2024-08-11 17:19:53 +0200

    Added cloudinary to heroku

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = ENV['dyciaxri0']
+  config.api_key = ENV['923446252637638']
+  config.api_secret = ENV['OQARmkp_jv_h63DOInMDlPnRJ6g']
+  config.secure = true
+end
